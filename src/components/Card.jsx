ceebe0da5538018c3fb78cdabb91ee714e1b47ee
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-23 21:40:56 +0300

    Add a card component for displaying blog posts.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,47 @@
+import { Link } from "react-router-dom";
+
 export const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
+      <Link to="#">
+        <img
+          className="rounded-t-lg"
+          src="/docs/images/blog/image-1.jpg"
+          alt=""
+        />
+      </Link>
+      <div className="p-5">
+        <Link to="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+        <a
+          href="#"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
