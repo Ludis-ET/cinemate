71a5d3d92bcd982359c791eece17debdbc28f17e
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-23 21:49:02 +0300

    Fetch movie data and display it in cards on the movie list page.

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,15 +1,19 @@
+import { useEffect, useState } from "react";
 import { Card } from "../components/Card";
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch();
+      const data = await response.json();
+      setMovies(data.results);
+    }
+  }, []);
+
   return (
     <main className="max-w-7xl mx-auto py-7 flex flex-wrap justify-start">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
       <Card />
     </main>
   );
