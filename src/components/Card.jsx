31e317d14b56e6728f243e3247d4b9dda3306d84
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-23 22:52:38 +0300

    Update the Card component to use movie data from an API and display movie-specific information.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,19 @@
 import { Link } from "react-router-dom";
 
-export const Card = () => {
+export const Card = ({ movie }) => {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
-      <Link to="#">
-        <img
-          className="rounded-t-lg"
-          src="/docs/images/blog/image-1.jpg"
-          alt=""
-        />
+      <Link to={`/movie/${movie._id}`}>
+        <img className="rounded-t-lg" src={movie.primaryImage.url} alt="" />
       </Link>
       <div className="p-5">
-        <Link to="#">
+        <Link to={`/movie/${movie._id}`}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            {movie.title}
           </h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+          {movie.overview}
         </p>
         <a
           href="#"
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -3,18 +3,30 @@ import { Card } from "../components/Card";
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
-
+  const url = "https://moviesdatabase.p.rapidapi.com/titles";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "1f10b6705bmshaf0b3cb273be1afp1490aejsn6c2625540b7d",
+      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+    },
+  };
   useEffect(() => {
     async function fetchMovies() {
-      const response = await fetch();
+      const response = await fetch(url, options);
       const data = await response.json();
       setMovies(data.results);
+      console.log(data);
+      console.log(movies.results.primaryImage);
     }
+    fetchMovies();
   }, []);
 
   return (
     <main className="max-w-7xl mx-auto py-7 flex flex-wrap justify-start">
-      <Card />
+      {movies.map((movie) => (
+        <Card key={movie._id} movie={movie} />
+      ))}
     </main>
   );
 };
