0e2176e2b028c34943a63111e477cb886a9dd4a5
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-23 17:42:58 +0300

    Update dependencies and add React Router

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+
+import { Card } from "./Card";
---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+import { Search } from "./Search";
+import { NotFound } from "./NotFound";
+import { MovieDetail } from "./MovieDetail";
+import { MovieList } from "./MovieList";
