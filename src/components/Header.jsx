619dc38b7edfbcf86f3130823be6f7e52c6081c4
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-23 21:16:42 +0300

    Update header component with dark mode toggle and search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.png";
 
 export const Header = () => {
   const [hidden, setHidden] = useState(true);
+  const [dark, setDark] = useState(
+    JSON.parse(localStorage.getItem("DarkMode")) || true
+  );
+
+  useEffect(() => {
+    localStorage.setItem("DarkMode", JSON.stringify(dark));
+    dark
+      ? document.documentElement.classList.remove("dark")
+      : document.documentElement.classList.add("dark");
+  }, [dark]);
 
   const activeClass =
     "text-base block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
@@ -22,6 +32,7 @@ export const Header = () => {
             Cinemate
           </span>
         </Link>
+
         <div className="flex md:order-2">
           <button
             type="button"
@@ -47,6 +58,43 @@ export const Header = () => {
               />
             </svg>
             <span className="sr-only">Search</span>
+          </button>
+          <button
+            type="button"
+            class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm p-2 me-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            onClick={() => setDark(!dark)}
+          >
+            {!dark ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+                />
+              </svg>
+            )}
           </button>
           <div className="relative hidden md:block">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -100,6 +148,7 @@ export const Header = () => {
             </svg>
           </button>
         </div>
+
         <div
           className={`items-center justify-between ${
             hidden ? "hidden" : ""
@@ -131,6 +180,7 @@ export const Header = () => {
               placeholder="Search..."
             />
           </div>
+
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <NavLink
