2d6bfafde4d14f0f8e54856739360d1a3d31cc4b
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-23 23:35:17 +0300

    Update API endpoint and use movie ID instead of movie _id.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 
 export const Card = ({ movie }) => {
+  const image = `https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`;
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
-      <Link to={`/movie/${movie._id}`}>
-        <img className="rounded-t-lg" src={movie.primaryImage.url} alt="" />
+      <Link to={`/movie/${movie.id}`}>
+        <img className="rounded-t-lg" src={image} alt="" />
       </Link>
       <div className="p-5">
-        <Link to={`/movie/${movie._id}`}>
+        <Link to={`/movie/${movie.id}`}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {movie.title}
           </h5>
@@ -15,8 +16,8 @@ export const Card = ({ movie }) => {
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {movie.overview}
         </p>
-        <a
-          href="#"
+        <Link
+          to={`/movie/${movie.id}`}
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Read more
@@ -35,7 +36,7 @@ export const Card = ({ movie }) => {
               d="M1 5h12m0 0L9 1m4 4L9 9"
             />
           </svg>
-        </a>
+        </Link>
       </div>
     </div>
   );
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -3,21 +3,23 @@ import { Card } from "../components/Card";
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
-  const url = "https://moviesdatabase.p.rapidapi.com/titles";
   const options = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "1f10b6705bmshaf0b3cb273be1afp1490aejsn6c2625540b7d",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NjEyZDllYTg3NDdmODc0OGU3NTFlMDY2MTdjNGZlZSIsInN1YiI6IjY1ZDhiYTk5ZDRkNTA5MDE4NjY1NDljZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LPgKaYjPmA29NlJoKXNoh_z7jozhAzXCOgt58Zhakdw",
     },
   };
   useEffect(() => {
     async function fetchMovies() {
-      const response = await fetch(url, options);
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing",
+        options
+      );
       const data = await response.json();
       setMovies(data.results);
       console.log(data);
-      console.log(movies.results.primaryImage);
     }
     fetchMovies();
   }, []);
@@ -25,7 +27,7 @@ export const MovieList = () => {
   return (
     <main className="max-w-7xl mx-auto py-7 flex flex-wrap justify-start">
       {movies.map((movie) => (
-        <Card key={movie._id} movie={movie} />
+        <Card key={movie.id} movie={movie} />
       ))}
     </main>
   );
