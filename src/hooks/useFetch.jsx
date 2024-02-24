63257ded5976cf670f7e5ada98e44ea8feea6c74
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-24 09:05:50 +0300

    Add API authorization token to the fetch request.

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 export const useFetch = (url, query = "") => {
   const [data, setData] = useState([]);
   const apiKey = import.meta.env.REACT_API_KEY;
+  const auth = import.meta.env.REACT_APP_API_AUTHORIZATION;
   const address = query
     ? `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`
     : `https://api.themoviedb.org/3/${url}`;
@@ -11,7 +12,7 @@ export const useFetch = (url, query = "") => {
     headers: {
       accept: "application/json",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NjEyZDllYTg3NDdmODc0OGU3NTFlMDY2MTdjNGZlZSIsInN1YiI6IjY1ZDhiYTk5ZDRkNTA5MDE4NjY1NDljZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LPgKaYjPmA29NlJoKXNoh_z7jozhAzXCOgt58Zhakdw",
+        {auth}
     },
   };
   useEffect(() => {
